c1c2542cdeda17ea10aadf06e41cbc76df42b4a5
edpantzar@gmail.com <edpantzar@gmail.com>
2019-09-22 14:17:15 -0500

    Fixed keypress emitting

--- index.js
+++ index.js
@@ -1,231 +1,30 @@
+#!/usr/bin/env node
 'use strict'; 
 
-/* eslint-env node */
 
-/* Things to keep in mind...
-*   - Need to set failure bounds based on gameFrame height and elements
-*   - Observables might be a good way to update the gameframe with bird position
-*   - Rendomization of frame must happen before bird placement
-*   - Difficulty can be determined by interval rate and randomization
-*   - Columns should be created with decreasing likelihood of generating a solid
-*   - !!! Improve choppiness by queueing frames between updates
-*/
-
-const rl = require('readline');
-const clear = require('clear');
-const fs = require('fs');
-const gameConfig = require('./config');
-
-let frame = [
-  ['☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️'],
-  ['☁️', '☁️', '☁️', '☁️', '☁️', ' ', ' ', '☁️', '☁️', '☁️', '☁️', '☁️', ' ', '☁️', '☁️','☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️'],
-  ['☁️', '☁️', '☁️', ' ', ' ', ' ', ' ', ' ', '☁️', '☁️', ' ', ' ', ' ', ' ', ' ',' ', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️', '☁️'],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', '🦆', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  ['⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', ' ', '⛰️', ' ', '⛰️', ' ', ' ', ' ', '⛰️', '⛰️','⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️'],
-  ['⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', ' ', '⛰️', '⛰️', '⛰️', ' ', ' ', '⛰️', '⛰️', '⛰️','⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️'],
-  ['⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️', '⛰️'],
-];
+const terminalDuck = require('./games/terminalDuck');
+const commander = require('commander');
+const inquirer = require('inquirer');
 
 
-let DUCK = gameConfig.environment.duck;
-let SKY = gameConfig.environment.sky;
-let GROUND = gameConfig.environment.ground;
-let X = gameConfig.game.start.position.x;
-let PREV_X = gameConfig.game.start.position.x;
-let Y = gameConfig.game.start.position.y;
-let FRAME_HEIGHT = gameConfig.environment.frameHeight;
-let FRAME_LENGTH = gameConfig.environment.frameLength;
-let SCORE = 0;
-let PREV_BLANK_MAX = gameConfig.environment.initialSpace.maxIndex || FRAME_HEIGHT - 1;
-let PREV_BLANK_MIN = gameConfig.environment.initialSpace.minIndex || 1;
+const program = commander;
+program.version('0.0.1');
+program.arguments('<cmd>');
 
-let GAME_DIFFICULTY = 'medium';
-let GAME_SPEED = gameConfig.game.difficulty[GAME_DIFFICULTY].frameRate;
-let FALL_SPEED = gameConfig.game.difficulty[GAME_DIFFICULTY].fallSpeed;
-
-const createInitialFrame = (height, width) => {
-  let initalFrame = [];
-  for(let i=height; i<=height; i++) {
-    for(let j=width; j<width; j++) {
-
-    } 
-  }
-}
-
-
-const collision = () => {
-  if (frame[(Y)][X+1] !== ' ' && frame[(Y)][X+1] !== '🦆'){
-    frame[Y][X] = '💥';
-    clear();
-    console.log('');
-    frame.forEach((line) => {
-      console.log(line.join(''));
+program.command('terminal-duck').action(() => { 
+  inquirer.prompt([
+      {
+        name: 'play',
+        type: 'confirm',
+        message: 'Would you like to play Terminal Duck'
+      }
+    ]).then((response) => {
+      if (response.play) {
+        terminalDuck();
+      }
     });
-    console.log('Murderer.');
-    console.log('Final score: ', SCORE);
-    process.exit(1);
-  }
-}
-
-const moveUp = () => {
-  let prevY = Y;
-  if (Y - 1 >= 0){
-    Y--;
-  } else {
-    return;
-  }
-  frame[Y][X] = '🦆';
-  frame[prevY][X] = ' ';
-  printFrame('up');
-}
-
-const moveDown = () => {
-  let prevY = Y;
-  if (Y < FRAME_HEIGHT) {
-    Y++;
-  } else {
-    return;
-  }
-  frame[Y][X] = '🦆';
-  frame[prevY][X] = ' ';
-  printFrame('down');
-}
-
-const getRandom = () => {
-  let flip = Math.ceil(Math.random() * 3);
-  switch (flip) {
-    case 1: return 1;
-    case 2: return -1;
-    case 3: return 0;
-    default: break;
-  }
-}
-
-const blankHeightPos = () => {
-
-  let nextBlankMin = getRandom() + PREV_BLANK_MIN;
-  if (nextBlankMin <= 1) {
-    nextBlankMin = 1;
-  }
-  if (nextBlankMin > FRAME_HEIGHT - 5) {
-    nextBlankMin = FRAME_HEIGHT - 5;
-  }
-
-  let nextBlankMax = getRandom() + PREV_BLANK_MAX;
-
-  //check that max is greater than min by at least 3
-  if (nextBlankMin + 3 >= nextBlankMax){
-    nextBlankMax = nextBlankMin + 3;
-  }
-
-  // check that max is never equal or greater to FRAME_HEIGHT
-  if (nextBlankMax >= FRAME_HEIGHT - 2) {
-    nextBlankMax = FRAME_HEIGHT - 2;
-    if (nextBlankMin + 3 >= nextBlankMax){
-      nextBlankMin--;
-    }
-  }
-
-
-  PREV_BLANK_MIN = nextBlankMin;
-  PREV_BLANK_MAX = nextBlankMax;
-  
-  return [nextBlankMin, nextBlankMax];
-}
-
-const run = () => {
-  let nextEnv = blankHeightPos();
-  let min = nextEnv[0];
-  let max = nextEnv[1];
-  let nextFrame = frame.map((el, index) => {
-    if (index === Y) {
-      if (index >= 0 && index <= min) {
-        el.splice(X, 1);
-        el.shift();
-        el.push('☁️');
-        el.splice(X, 0, '🦆');
-      }
-      if (index > min && index <= max) {
-        el.splice(X, 1);
-        el.shift();
-        el.push(' ');
-        el.splice(X, 0, '🦆');
-      }
-      if (index > max && index < FRAME_HEIGHT) {
-        el.splice(X, 1);
-        el.shift();
-        el.push('⛰️');
-        el.splice(X, 0, '🦆');
-        }
-      } else {
-        if (index >= 0 && index <= min) {
-          el.shift();
-          el.push('☁️');
-        }
-        if (index > min && index <= max) {
-            el.shift();
-            el.push(' ');
-        } 
-        if (index > max && index < FRAME_HEIGHT) {
-          el.shift();
-          el.push('⛰️');
-        }
-    }
-    return el;
-  });
-  collision();
-  frame = nextFrame;
-  printFrame();
-  SCORE++;
-  // accelerateFR();
-}
-
-const swapRight = () => {
-  PREV_X = frame[Y][X + 1];
-  frame[Y][X + 1] = '🦆';
-  frame[Y][X] = PREV_X;
-  printFrame();
-}
-
-const swapLeft = () => {
-  PREV_X = frame[Y][X - 1];
-  frame[Y][X - 1] = '🦆';
-  frame[Y][X] = PREV_X;
-  printFrame();
-}
-
-const printFrame = () => {
-  clear();
-  console.log('');
-  frame.forEach((line) => {
-    console.log(line.join(''));
-  });
-  collision();
-  console.log('score: ' + SCORE);
-}
-
-setInterval(run, GAME_SPEED);
-setInterval(moveDown, FALL_SPEED);
-
-process.stdin.setRawMode(true);
-
-rl.emitKeypressEvents(process.stdin);
-
-process.stdin.on('keypress', (str, key) => {
-  switch(key.name) {
-    case 'q': process.exit(0); break;
-    case 'right': swapRight(); X++; break;
-    case 'left': swapLeft(); X--; break;
-    case 'up': moveUp(); break;
-    case 'down': moveDown(); break;
-    default: break;
-  }
 });
+
+program.parse(process.argv);
+
+module.exports = program;
